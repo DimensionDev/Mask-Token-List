00a0523178db37f96016089e75ea6283ee2f91c9
billy <Billy@mask.io>
2021-11-08 18:12:28 +0800

    fix(*): optimization validator

--- src/validator/erc20/validator.js
+++ src/validator/erc20/validator.js
@@ -1,19 +1,26 @@
 const fetch = require("node-fetch");
+const https = require("https");
 const localList = require("./erc20-list.json");
+const existingERC20Addressed = new Set();
+
 let listCache = {};
 const getERC20List = async () => {
+  if (listCache && listCache.data) return listCache;
   try {
     const url =
       "https://web-api.coinmarketcap.com/v1/cryptocurrency/map?aux=status,platform&listing_status=active,untracked&sort=cmc_rank";
-    const res = await fetch(url);
-    listCache = await res.json();
-    return listCache
+    const options = {
+      agent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    };
+    const res = await fetch(url, options);
+    listCache = await res.json().data;
   } catch (e) {
-    return e
+    listCache = localList;
   }
+  return listCache;
 };
-
-// getERC20List();
 
 /**
  * @param { Array<{name:string, id: string, symbol:string, address: string}> } tokenList
@@ -21,12 +28,15 @@ const getERC20List = async () => {
 const validateExistenceCMC = async (tokenList) => {
   let res = 1;
   const errArr = [];
-  const existingERC20Addressed = new Set();
-  localList.data.forEach((item) => {
-    if (item.platform && item.platform.token_address) {
-      existingERC20Addressed.add(item.platform.token_address.toLowerCase());
-    }
-  });
+  const list = await getERC20List();
+  if (!existingERC20Addressed.size) {
+    list.data.forEach((item) => {
+      if (item.platform && item.platform.token_address) {
+        existingERC20Addressed.add(item.platform.token_address.toLowerCase());
+      }
+    });
+  }
+
   tokenList.forEach((token) => {
     if (!existingERC20Addressed.has(token.address.toLowerCase())) {
       errArr.push(token);
